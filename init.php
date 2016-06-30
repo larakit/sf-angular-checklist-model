8d98cb72068f8e57185703bebce39a421f116151
Alexey Berdnikov <aberdnikov@gmail.com>
2016-06-30 14:17:43 +0500

    fixed

--- init.php
+++ init.php
@@ -2,4 +2,4 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-checklist-model')
     ->usePackage('larakit/sf-angular')
     ->ngModule('checklist-model')
-    ->js('cdnjs.cloudflare.com/ajax/libs/checklist-model/0.9.0/checklist-model.min.js');
+    ->js('//cdnjs.cloudflare.com/ajax/libs/checklist-model/0.9.0/checklist-model.min.js');
