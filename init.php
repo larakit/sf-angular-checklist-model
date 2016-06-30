cd1005fc1e60be4c60b6f68ee0d6f8968e8afb2c
Berdnikov Alexey <aberdnikov@gmail.com>
2016-06-30 11:03:59 +0500

    Create init.php

---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+\Larakit\StaticFiles\Manager::package('larakit/sf-angular-checklist-model')
+    ->usePackage('larakit/sf-angular')
+    ->ngModule('checklist-model')
+    ->js('cdnjs.cloudflare.com/ajax/libs/checklist-model/0.9.0/checklist-model.min.js');
