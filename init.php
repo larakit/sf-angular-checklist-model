44c1d13b6b22e6c58710c6cb171b151d67dce53d
Berdnikov Alexey <aberdnikov@gmail.com>
2017-04-20 09:02:29 +0500

    Update init.php

--- init.php
+++ init.php
@@ -2,5 +2,4 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-checklist-model')
     ->usePackage('larakit/sf-angular')
     ->setSourceDir('public')
-    ->ngModule('checklist-model')
     ->jsPackage('checklist-model.min.js');
